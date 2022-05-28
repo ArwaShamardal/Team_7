d820702245ba6cdc19b633d115226ed414620698
mohammedzaki7 <63197575+mohammedzaki7@users.noreply.github.com>
2022-05-28 06:20:52 +0200

    added function that makes sure name is not in current or parent table and made modifications on creating the table

--- Compiler/bngrb.c
+++ Compiler/bngrb.c
@@ -18,18 +18,52 @@ typedef struct entry
 
 } entry ;
 
-int size = 200;
-
-entry** CreateTable()
+typedef struct table
 {
+    entry** tableOfSymbols;
+    int id, parentId;
+    bool created;
+}   table;
+
+int size = 200;
+int tableID = 0;
+int parentID = 0;
+bool tableCreated = 0;
+
+table* TABLES;
+int CreateTable()
+{
+    if(tableCreated == false)
+        {
+            tableCreated = true;
+            TABLES = malloc(sizeof( struct table* ) * size );  // creating the list of tables
+        }
+    tableID++; // index of tables is starting from 1
+    
 
 	entry** table = malloc( sizeof( struct entry* ) * size );    // Hashmap has 200 entries 
     for( int i = 0; i < size; i++ )
 	{
 		table[i] = NULL;
 	}
+    // struct table createdTable;
+    // createdTable.tableOfSymbols = table;
+    // createdTable.id = tableID;
+    // createdTable.parentId = parentID;
+    TABLES[tableID].id = tableID;
+    TABLES[tableID].parentId = parentID;
+    TABLES[tableID].tableOfSymbols= table;
+    TABLES[tableID].created = true;
 
-	return table;
+
+	return TABLES[tableID].id;
+}
+
+int ExitingTable()
+{
+    int exitingTableID;
+    exitingTableID = TABLES[tableID].parentId;
+    return exitingTableID;
 }
 
 entry * create_entry( char *name, int value, int data_type)
@@ -53,34 +87,75 @@ entry * create_entry( char *name, int value, int data_type)
 	return new_entry;
 }
 
-int InsertInHashMap(struct entry** table, struct entry* e)
+int FoundInCurrentAndParents(struct table symbolsTable, struct entry* e, int indx)
 {
-	// int hash = 0;
-   	// int i = 0;
-	// char* givenString = e->name;
-    // while(givenString[i] != '\0')
-    // {
-    //     hash = (hash + givenString[i]) % size;
-    //     i++;
-    // }
-
-	int indx = 0;
-
-	if(table[indx] == NULL)
+    //table currentTable = symbolsTable;
+    while (TABLES[symbolsTable.id].created == true)
     {
-        table[indx] = e;
-        printf("Entered the index");
+        //printf("current table id is %d\n", symbolsTable.id);
+        //printf("parent table id is %d\n", symbolsTable.parentId);
+        //printf("searching for %s\n", e->name);
+        
+        //printf("index is %d\n", indx);
+        char *findingChar = e->name;
+       
+        if (TABLES[symbolsTable.id].tableOfSymbols[indx] != NULL)
+        {
+            printf("%s\n",findingChar);
+            printf("index is %d\n", indx);
+            printf("goog %s \n", TABLES[symbolsTable.id].tableOfSymbols[indx]->name );
+            if(strcmp(findingChar, TABLES[symbolsTable.id].tableOfSymbols[indx]->name) == 0)
+            {
+                printf("this variable %s is defined at the parent or the current table\n" , findingChar);
+                return 1;
+            }
+        }
+        //currentTable.parentId= TABLES[currentTable.parentId].parentId;
+        symbolsTable.id = TABLES[symbolsTable.id].parentId;
+        
     }
-	else
-	{
-		struct entry* e2 = table[indx];
-		while(e2->next != NULL)
-		{
-			e2 = e2->next;
-		}
-		e2->next = e;
-	}
+    return 0;
+    
 }
+
+int InsertInHashMap(struct table tableOfSymbols, struct entry* e)
+{
+    // HASHING PART
+    //printf("current table id is %d\n", tableOfSymbols.id);
+    //    printf("parent table id is %d\n", tableOfSymbols.parentId);
+	int hash = 0;
+   	int i = 0;
+	char* givenString = e->name;
+    while(givenString[i] != '\0')
+    {
+        hash = (hash + givenString[i]) % size;
+        i++;
+    }
+
+	int indx = hash;
+    if (FoundInCurrentAndParents(tableOfSymbols, e, indx) == 0)
+    {
+        if(tableOfSymbols.tableOfSymbols[indx] == NULL)
+        {
+            //printf("insert index is %d\n", indx);
+            tableOfSymbols.tableOfSymbols[indx] = e;
+            //printf("didn't enter this index before\n");
+        }
+	    else
+	    {
+            struct entry* e2 = tableOfSymbols.tableOfSymbols[indx] ;
+	        while(e2->next != NULL)
+	    	{
+	    	    e2 = e2->next;
+	    	}
+	        e2->next = e;
+		
+	    }
+    }    
+	
+}
+
+
 
 // int insert(char name, int value, int data_type, struct entry** table){
 //     struct entry myEntry =  create_entry(  name,  value,  data_type);
@@ -88,17 +163,33 @@ int InsertInHashMap(struct entry** table, struct entry* e)
 // }
 
 int main(){
-	struct entry ** current_table = CreateTable();
-	struct entry* currentEntry = create_entry("dad", 3, 2);
-	struct entry* currentEntry2 = create_entry("mum", 3, 2);
+	int myTableID =  CreateTable();
+	struct entry* currentEntry = create_entry("zdad", 3, 2);
+	struct entry* currentEntry2 = create_entry("y", 3, 2);
+    int myTableID2 = CreateTable();
+     TABLES[myTableID2].parentId = 1;
+    struct entry* currentEntry3 = create_entry("y", 3, 2);
+	struct entry* currentEntry4 = create_entry("zdad", 3, 2);
+    //struct table current_table = CreateTable();
+     
 
-    
-    //printf("%s",currentEntry->name);
 
-	InsertInHashMap(current_table, currentEntry);
-	InsertInHashMap(current_table, currentEntry2);
+    //printf("id is %d\n", TABLES[TABLES[2].parentId].parentId);
+	InsertInHashMap(TABLES[myTableID], currentEntry);
+    printf("moonn\n");
+	InsertInHashMap(TABLES[myTableID], currentEntry2);
+printf("knight\n");
+    InsertInHashMap(TABLES[myTableID2], currentEntry3);
+	InsertInHashMap(TABLES[myTableID2], currentEntry4);
+    // printf("%s \n",TABLES[myTableID].tableOfSymbols[0]->name);
+    // printf("%s \n",TABLES[myTableID].tableOfSymbols[0]->next->name);
 
+    // printf("%s \n",TABLES[myTableID2].tableOfSymbols[0]->name);
+    // printf("%s \n",TABLES[myTableID2].tableOfSymbols[0]->next->name);
 
-    printf("%s",current_table[0]->name);
-    printf("%s",current_table[0]->next->name);
+    // printf("%d \n",TABLES[myTableID].id);
+    // printf("%d \n",TABLES[myTableID].parentId);
+
+    // printf("%d \n",TABLES[myTableID2].id);
+    // printf("%d \n",TABLES[myTableID2].parentId);
 }
--- Compiler/semanticAnalyzer.h
+++ Compiler/semanticAnalyzer.h
@@ -1,6 +1,7 @@
 #ifndef SEMANTICANALYZER_H
 #define SEMANTICANALYZER_H 
-#define size  200
+#define sizeTable  200
+#define numTables 20 
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -22,22 +23,77 @@ typedef struct entry
 
 } entry ;
 
-//int size = 200;
-int tableID = 0;
-
-entry** CreateTable()
+typedef struct table
 {
-	tableID++;
-	entry** table = malloc( sizeof( struct entry* ) * size );    // Hashmap has 200 entries 
-    for( int i = 0; i < size; i++ )
+    entry** tableOfSymbols;
+    int id, parentId;
+    bool created;
+}   table;
+
+int tableID = 0;
+int parentID = 0;
+bool tableCreated = 0;
+
+table* TABLES;
+
+int CreateTable()
+{
+    if(tableCreated == false)
+        {
+            tableCreated = true;
+            TABLES = malloc(sizeof( struct table* ) * numTables );  // creating the list of tables
+        }
+    tableID++; // index of tables is starting from 1
+    
+
+	entry** table = malloc( sizeof( struct entry* ) * sizeTable );    // Hashmap has 200 entries 
+    for( int i = 0; i < sizeTable; i++ )
 	{
 		table[i] = NULL;
 	}
+    // struct table createdTable;
+    // createdTable.tableOfSymbols = table;
+    // createdTable.id = tableID;
+    // createdTable.parentId = parentID;
+    TABLES[tableID].id = tableID;
+    TABLES[tableID].parentId = parentID;
+    TABLES[tableID].tableOfSymbols= table;
+    TABLES[tableID].created = true;
 
-	return table;
+
+	return TABLES[tableID].id;
 }
 
-bool InsertInHashMap(struct entry** table, struct entry* e)
+int ExitingTable()  // not sure of that function 
+{
+    int exitingTableID;
+    exitingTableID = TABLES[tableID].parentId;
+    return exitingTableID;
+}
+
+int FoundInCurrentAndParents(struct table symbolsTable, struct entry* e, int indx)
+{
+    while (TABLES[symbolsTable.id].created == true)
+    {
+
+        char *findingChar = e->name;
+       
+        if (TABLES[symbolsTable.id].tableOfSymbols[indx] != NULL)
+        {
+            if(strcmp(findingChar, TABLES[symbolsTable.id].tableOfSymbols[indx]->name) == 0)
+            {
+                printf("this variable %s is defined at the parent or the current table\n" , findingChar);
+                return 1;
+            }
+        }
+        symbolsTable.id = TABLES[symbolsTable.id].parentId;
+        
+    }
+    return 0;
+    
+}
+
+bool InsertInHashMap(struct table tableOfSymbols, struct entry* e)
 {
 	bool inserted = false;
 	int hash = 0;
@@ -45,31 +101,34 @@ bool InsertInHashMap(struct entry** table, struct entry* e)
 	char* givenString = e->name;
     while(givenString[i] != '\0')
     {
-        hash = (hash + givenString[i]) % size;
+        hash = (hash + givenString[i]) % sizeTable;
         i++;
     }
 
 	int indx = hash;
 
-	if(table[indx] == NULL)
+	if (FoundInCurrentAndParents(tableOfSymbols, e, indx) == 0)
     {
-        table[indx] = e;
-        //printf("Entered the index");
+        if(tableOfSymbols.tableOfSymbols[indx] == NULL)
+        {
+            tableOfSymbols.tableOfSymbols[indx] = e;
+        }
+	    else
+	    {
+            struct entry* e2 = tableOfSymbols.tableOfSymbols[indx] ;
+	        while(e2->next != NULL)
+	    	{
+	    	    e2 = e2->next;
+	    	}
+	        e2->next = e;
+		
+	    }
 		inserted = true;
-    }
-	else
-	{
-		struct entry* e2 = table[indx];
-		while(e2->next != NULL)
-		{
-			e2 = e2->next;
-		}
-		e2->next = e;
-		inserted = true;
-	}
+    }    
 
 	return inserted;
 }
+
 
 
 
@@ -98,10 +157,10 @@ entry * create_entry( char *name, int value, int data_type)
 	return new_entry;
 }
 
-struct entry* insert(char *name, int value, int data_type, struct entry** table)
+struct entry* insert(char *name, int value, int data_type, struct table currentTable)
 {
     struct entry *myEntry =  create_entry(  name,  value,  data_type);
-    bool insertedSuccessfully = InsertInHashMap(table, myEntry);
+    bool insertedSuccessfully = InsertInHashMap(currentTable, myEntry);
 	if(insertedSuccessfully == true)
 		return myEntry;
 	else 
